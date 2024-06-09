97d25765d1d3a05e735972bb13f100590bd48479
percyzavala <percyzavala@gmail.com>
2024-06-08 22:51:50 -0500

    Se implemento companente App.tsx con codigo html y se paso referencia a componente hijo MenuItem.tsx y se creo interface para el parametro

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MenuItem } from './components/MenuItem';
 import { menuItems } from './database/data';
 
 
@@ -12,8 +13,17 @@ function App() {
       <h1 className=" text-center text-4xl font-black">Propina Rapida</h1>
     </header>
     <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-      <div className="">
-        <h2>menu</h2>
+      <div className="p-5">
+        <h2 className="text-4xl font-black" >menu</h2>
+
+        <div className=" space-y-3 mt-10">
+
+        {
+          menuItems.map(item=>(
+            <MenuItem key={item.id} item={item} />
+            ))
+            }
+        </div>
       </div>
       <div className="">
         <h2>Consumo</h2>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,18 @@
-export const MenuItem = () => {
+import { MenuList } from '../types/tipos';
+
+interface item {
+  item:MenuList
+}
+
+export const MenuItem = ({item}:item) => {
+  console.log("desde menu item",item);
+  
   return (
-    <div>MenuItem</div>
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between">
+    <p>{item.name}</p>
+    <p>S/. {item.price}</p>
+
+
+    </button>
   )
 }
